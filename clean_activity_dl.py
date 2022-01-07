958fc7450c36f34905c64313bff0e61e4c50abcc
RyanAugust <rad19206@yahoo.com>
2022-01-07 06:55:56 -0800

    add in logic to matche between muted dir and active import dir

--- clean_activity_dl.py
+++ clean_activity_dl.py
@@ -10,3 +10,15 @@ for dir_,dirs,files in  os.walk(activities_dir):
 				os.remove(dir_ + "/" + file)
 			else:
 				os.remove(activities_dir + "/" + file)
+
+### Ensure muted are not in the active dir
+activity_dir_files = files
+
+muted_dir =
+
+for dir_,dirs,files in os.walk(muted_dir):
+	for file in files:
+		if file not in activity_dir_files:
+			print(file)
+import time
+time.sleep(60)
