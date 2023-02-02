45cf96adf923af84d0ab4162791ca48ab39a5d29
RyanAugust <rad19206@yahoo.com>
2023-02-02 06:53:08 -0800

    remove double delete and comment on final choice function

--- clean_activity_dl.py
+++ clean_activity_dl.py
@@ -14,16 +14,17 @@ ctime_lookback = 60*4
 min_ctime = max_ctime - ctime_lookback
 
 
-for file in dl_files:
-    if '.fit' not in file.name:
-        os.remove(file.path)
-    elif os.path.getctime(file.path) <  min_ctime:
-        os.remove(file.path)
+# for file in dl_files:
+#     if '.fit' not in file.name:
+#         os.remove(file.path)
+#     elif os.path.getctime(file.path) <  min_ctime:
+#         os.remove(file.path)
 
 ### Ensure muted are not in the active dir
 muted_dir = """C:/Users/ryand/Golden Cheetah Dir/dl_activities/muted_activities"""
 old_dir = """C:/Users/ryand/Golden Cheetah Dir/dl_activities/older_activities"""
 
+## Gather file lists from old and muted folders
 mute_files = []
 for file in os.scandir(muted_dir):
     # add in only files with extension. No dirs
@@ -35,10 +36,16 @@ for file in os.scandir(old_dir):
     if "." in file.path:
         old_files.append(file.name)
 
+
 for file in dl_files:
     if '.fit' not in file.name:
+        ## Remove non .fit files. Don't need em
         os.remove(file.path)
+
     if (file.name in mute_files) or (file.name in old_files):
+        ## Delete files that have already been moved to mute and old dirs
         os.remove(file.path)
+
     elif os.path.getctime(file.path) <  min_ctime:
+        ## Move files that have not been moved to mute and old dirs
         os.rename(file.path, '/'.join(old_dir,file.name))
