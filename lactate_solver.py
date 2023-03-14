049505f97b84ae945bf0a56ad3205be8cbe93705
RyanAugust <rad19206@yahoo.com>
2023-03-14 14:03:45 -0700

    build to class with multiple solvers

--- lactate_solver.py
+++ lactate_solver.py
@@ -2,51 +2,88 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# read in lactate values
-df = pd.read_excel("C:\\Users\\ryand\\Downloads\\Athlete_Examples.xlsx",sheet_name="RyanDryer")
-# df = pd.read_excel("C:\\Users\\ryand\\Downloads\\Athlete_Examples.xlsx",sheet_name="Example")
-pwr = df['power'].to_numpy()
-lct = df['lactate'].to_numpy()
-hr = df['heartrate'].to_numpy()
+class lactate_solver(object):
+    def __init__(self, lactate_vector, heartrate_vector ,power_vector):
+        self.lactate_vector = lactate_vector
+        self.heartrate_vector = heartrate_vector
+        self.power_vector = power_vector
+        self.solver_methods = {'2and4':self.method_2and4mmol,
+                               '1+2':self.method_1plus2}
 
-p3 = np.poly1d(np.polyfit(x=pwr, y=lct, deg=3))
+    def find_LT_values(self, method_name):
+        method_function = self.solver_methods[method_name]
+        method_output = method_function()
 
-x_new = np.linspace(start=pwr.min() ,stop=pwr.max() ,num=200)
-lac_fit = p3(x_new)
+        method_output_dict = {'LT1_mmol':method_output[0],
+                            'LT1_power':method_output[1],
+                            'LT1_hr':method_output[2],
+                            'LT2_mmol':method_output[3],
+                            'LT2_power':method_output[4],
+                            'LT2_hr':method_output[5],
+                            'LTfit':method_output[6]}
 
-# Find LT1 & LT2
-## LT1
-LT_1_idx = (lac_fit > (lct.min() + 1)).argmax()
-print(LT_1_idx)
-LT_1 = lac_fit[LT_1_idx]
-LT_1_p = x_new[LT_1_idx]
+        return method_output_dict
 
-## LT2
-LT_2_2mmol_idx = (lac_fit > (LT_1 + 2)).argmax()
-LT_2 = lac_fit[LT_2_2mmol_idx]
-LT_2_p = x_new[LT_2_2mmol_idx]
+    def method_1plus2(self):
+        LTfit = np.poly1d(np.polyfit(x=self.power_vector, y=self.lactate_vector, deg=3))
+        x_new = np.linspace(start=self.power_vector.min() ,stop=self.power_vector.max() ,num=200)
+        lac_fit = LTfit(x_new)
 
-fig, ax = plt.subplots(1,1, dpi=200, figsize=(10,6))
-ax.scatter(x=pwr, y=lct, color='blue', label='Lactate Values')
-ax.plot(x_new, lac_fit, color='lightblue', label='Lactate Curve')
-ax1 = ax.twinx()
-ax1.scatter(x=pwr, y=hr, color='red', label='Heartrate Values')
+        # Modeled fit to HR
+        HRfit = np.poly1d(np.polyfit(x=self.power_vector, y=self.heartrate_vector, deg=3))
+        hr_fit = HRfit(x_new)
 
-## Reference Lines
-ax.axhline(y=LT_1, ls='--', lw=1, color='black', label='LT1')
-ax.axvline(x=LT_1_p, ls='--', lw=1, color='black')
-ax.annotate(xy=(LT_1_p + 1,LT_1 + 1), text=f'LT1:\n{LT_1:0.2f}mmol/L\n{LT_1_p:0.0f}w')
-ax.axhline(y=LT_2, ls=':', lw=1, color='black', label='LT2')
-ax.axvline(x=LT_2_p, ls=':', lw=1, color='black')
-ax.annotate(xy=(LT_2_p + 1,LT_2 + 1), text=f'LT2:\n{LT_2:0.2f}mmol/L\n{LT_2_p:0.0f}w')
+        # Find LT1 & LT2
+        ## LT1
+        LT_1_idx = (lac_fit > (self.lactate_vector.min() + 1)).argmax()
+        print(LT_1_idx)
+        LT1_mmol = lac_fit[LT_1_idx]
+        LT1_power = x_new[LT_1_idx]
+        LT1_hr = hr_fit[LT_1_idx]
+
+        ## LT2
+        LT_2_idx = (lac_fit > (LT1_mmol + 2)).argmax()
+        LT2_mmol = lac_fit[LT_2_idx]
+        LT2_power = x_new[LT_2_idx]
+        LT2_hr = hr_fit[LT_2_idx]
+    
+        return LT1_mmol, LT1_power, LT1_hr, LT2_mmol, LT2_power, LT2_hr, LTfit
+    
+    def method_2and4mmol(self):
+        LT1_mmol, LT1_power, LT1_hr, LT2_mmol, LT2_power, LT2_hr, LTfit = 0
+        
+        return LT1_mmol, LT1_power, LT1_hr, LT2_mmol, LT2_power, LT2_hr, LTfit
+
+    # def method_dmax(self):
+        
+        
+    #     return LT1_mmol, LT1_power, LT1_hr, LT2_mmol, LT2_power, LT2_hr, LTfit
+    
+    def plotter(self, LT1_mmol, LT1_power, LT1_hr, LT2_mmol, LT2_power, LT2_hr, LTfit, save=False):
+        x_new = np.linspace(start=self.power_vector.min() ,stop=self.power_vector.max() ,num=200)
+        lac_fit = LTfit(x_new)
+
+        fig, ax = plt.subplots(1,1, dpi=200, figsize=(10,6))
+        ax.scatter(x=self.power_vector, y=self.lactate_vector, color='blue', label='Lactate Values')
+        ax.plot(x_new, lac_fit, color='lightblue', label='Lactate Curve')
+        ax1 = ax.twinx()
+        ax1.scatter(x=self.power_vector, y=self.heartrate_vector, color='red', label='Heartrate Values')
+
+        ## Reference Lines
+        ax.axhline(y=LT1_mmol, ls='--', lw=1, color='black', label='LT1')
+        ax.axvline(x=LT1_power, ls='--', lw=1, color='black')
+        ax.annotate(xy=(LT1_power + 1,LT1_mmol + 1), text=f'LT1:\n{LT1_mmol:0.2f}mmol/L\n{LT1_power:0.0f}w')
+        ax.axhline(y=LT2_mmol, ls=':', lw=1, color='black', label='LT2')
+        ax.axvline(x=LT2_power, ls=':', lw=1, color='black')
+        ax.annotate(xy=(LT2_power + 1,LT2_mmol + 1), text=f'LT2:\n{LT2_mmol:0.2f}mmol/L\n{LT2_power:0.0f}w')
 
 
-ax.set_xlabel('Power')
-ax.set_ylabel('Lactate Concentration (mmol/L)')
-ax1.set_ylabel('Heartrate (bpm)')
-ax.set_ylim(0,lct.max()*1.25)
+        ax.set_xlabel('Power')
+        ax.set_ylabel('Lactate Concentration (mmol/L)')
+        ax1.set_ylabel('Heartrate (bpm)')
+        ax.set_ylim(0,self.lactate_vector.max()*1.25)
 
-
-fig.legend()
-
-plt.savefig('plot.png') 
+        fig.legend()
+        if save:
+            plt.savefig('plot.png') 
+        return fig
